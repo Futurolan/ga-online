8477fefe7336e0ec6dc585c1296d1169782873dd
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-03-25 18:05:21 +0100

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -3,7 +3,7 @@ const menu = [
   { type: 'config', id: 'news' },
   // { type: 'config', id: 'live' },
   { type: 'config', id: 'tournaments' },
-  { type: 'config', id: 'info'},
+  { type: 'config', id: 'info' },
   { type: 'page', title: 'FAQ', id: 200391, link: '/faq' },
   { type: 'config', id: 'partners' }
 ]
