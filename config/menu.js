34ed3ea272f4cda092cd5f8d73182b6df9eeda45
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-03-25 17:39:19 +0100

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -5,13 +5,8 @@ const menu = [
   { type: 'config', id: 'tournaments' },
   {
     type: 'nolink',
-    title: 'Infos',
-    children: [
-      { type: 'config', id: 'info' },
-      // { type: 'config', id: 'schedule' },
-      { type: 'page', title: 'FAQ', id: 200391, link: '/faq' }
-      // { type: 'page', title: 'Une page', id: 1246, link: '/grand-poitiers' }
-    ]
+    type: 'config', id: 'info',
+    type: 'page', title: 'FAQ', id: 200391, link: '/faq'
   },
   { type: 'config', id: 'partners' }
 ]
