d77dc1a681c6185ff37a40cfc1b63762099fa4df
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-04-06 17:27:28 +0200

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -1,7 +1,7 @@
 /* eslint-disable indent */
 const menu = [
   { type: 'config', id: 'news' },
-  // { type: 'config', id: 'live' },
+  { type: 'config', id: 'live' },
   { type: 'config', id: 'tournaments' },
   { type: 'config', id: 'info' },
   { type: 'page', title: 'FAQ', id: 200391, link: '/faq' }
